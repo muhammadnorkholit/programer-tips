8900e206d2955b3a946ca8715b7e8064d5801b45
Muhammad kholit <99956928+muhammadnorkholit@users.noreply.github.com>
2022-09-05 12:31:59 +0700

    Update live-search-js.js
    add comment

--- live-search-js.js
+++ live-search-js.js
@@ -1,13 +1,21 @@
 
+// mengambil elemenet input dengan id
 document.querySelector("#s").addEventListener("input", function () {
+//             mengambil elemenet list yang memiliki class data
             let data = document.querySelectorAll(".data")
+//             melooping element list dengan foreach
             data.forEach(e => {
+//              mengambil value dari element inputan dan merubahnya menjadi kecil dengan function toLowerCase
                 let data = document.querySelector("#s").value.toLowerCase()
+//                 mengambil semua content yang berisi text dari element yang di looping
                 let key = e.textContent
 
+//                 melakukan pengecekan dengan menggunakan method incudes()   
                 if (key.toLowerCase().includes(data.toLowerCase())) {
+//                             jika ada style display di kosongi
                     e.style.display = ""
                 } else {
+//                             jika tidak ada display menjadi none
                     e.style.display = "none"
 
                 }
